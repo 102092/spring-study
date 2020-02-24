864963ce23445b42f7149e0993e2b66878946ec0
KIM DONG HWAN <quaint3@gmail.com>
2020-02-24 23:18:05 +0900

    Study : SpringFramework 완강. 프로젝트 파일 정리

---
+++ spring-framework/demospringspel/src/main/java/com/demospringspel/demo/EventService.java
@@ -0,0 +1,13 @@
+package com.demospringspel.demo;
+
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EventService {
+
+  @NonNull
+  public String createEvent(@NonNull String name){
+    return "hello " + name;
+  }
+}
