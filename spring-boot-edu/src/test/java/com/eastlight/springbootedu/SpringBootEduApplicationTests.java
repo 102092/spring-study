17446047ea1b2afa07410a58fbf6bcbab54d9f01
KIM DONG HWAN <quaint3@gmail.com>
2020-02-26 16:52:21 +0900

    Springboot : project init

---
+++ spring-boot-edu/src/test/java/com/eastlight/springbootedu/SpringBootEduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eastlight.springbootedu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEduApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
