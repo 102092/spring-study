0012a9c23a3e5e9874435e40c0c7676a6701a025
KIM DONG HWAN <quaint3@gmail.com>
2020-03-11 21:50:31 +0900

    step1 : 내장웹서버 이해
    내장 톰켓 생성, 실행

--- spring-boot/src/main/java/com/sbedu/demo/DemoApplication.java
+++ spring-boot/src/main/java/com/sbedu/demo/DemoApplication.java
@@ -1,15 +1,40 @@
 package com.sbedu.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
 
-@SpringBootApplication
 public class DemoApplication {
 
-  public static void main(String[] args) {
-    SpringApplication application = new SpringApplication(DemoApplication.class);
-    application.setWebApplicationType(WebApplicationType.NONE);
-    application.run(args);
+  public static void main(String[] args) throws LifecycleException {
+    Tomcat tomcat = new Tomcat();
+    tomcat.setPort(8080);
+
+    Context context = tomcat.addContext("/", "/");
+
+    HttpServlet servlet = new HttpServlet() {
+      @Override
+      protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+          throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.println("<html><head><title>");
+        writer.println("Hello, Tomcat");
+        writer.println("</title></head>");
+        writer.println("<body><h1>Hello, World!</h1></body>");
+        writer.println("</html>");
+      }
+    };
+    String servletName = "helloServlet";
+    tomcat.addServlet("/", servletName, servlet);
+    context.addServletMappingDecoded("/hello", servletName);
+
+    tomcat.start();
+    tomcat.getServer().await();
   }
 }
