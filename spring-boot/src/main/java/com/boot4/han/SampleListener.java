1cbbe8700adeab55a830a09f3dc2816562b46ab6
KIM DONG HWAN <quaint3@gmail.com>
2020-03-30 22:52:22 +0900

    외부설정 2-1부

--- spring-boot/src/main/java/com/boot4/han/SampleListener.java
+++ spring-boot/src/main/java/com/boot4/han/SampleListener.java
@@ -1,5 +1,6 @@
 package com.boot4.han;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -8,13 +9,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class SampleListener implements ApplicationRunner {
 
-  @Value("${han.name}")
-  private String name;
+  @Autowired
+  HanProperties hanProperties;
 
   @Override
   public void run(ApplicationArguments args) {
     System.out.println("==============");
-    System.out.println(name);
+    System.out.println(hanProperties.name);
+    System.out.println(hanProperties.age);
+    System.out.println(hanProperties.fullName);
     System.out.println("==============");
 
   }
