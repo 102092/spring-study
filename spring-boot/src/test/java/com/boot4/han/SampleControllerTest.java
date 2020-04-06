139ef447199ff73bb9650c3d2912875c9f36b330
Han <quaint3@gmail.com>
2020-04-06 23:53:54 +0900

    테스트 : slicing test

--- spring-boot/src/test/java/com/boot4/han/SampleControllerTest.java
+++ spring-boot/src/test/java/com/boot4/han/SampleControllerTest.java
@@ -1,32 +1,30 @@
 package com.boot4.han;
 
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureMockMvc
+@WebMvcTest(SampleController.class)
 class SampleControllerTest {
-
-  @Autowired
-  WebTestClient webTestClient;
 
   @MockBean
   SampleService sampleService;
 
+  @Autowired
+  MockMvc mockMvc;
+
   @Test
-  public void hello() {
+  public void hello() throws Exception {
     when(sampleService.getName()).thenReturn("han");
 
-    webTestClient.get().uri("/hello").exchange()
-        .expectStatus().isOk()
-        .expectBody(String.class)
-        .isEqualTo("hello han");
+    mockMvc.perform(get("/hello"))
+        .andExpect(content().string("hello han"));
   }
 
 
